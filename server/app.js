0efb62b7d780f25b57ff4bb43be8d6c5c6b27fcd
Pragati Dugar <pragati.dugar@nagarro.com>
2020-04-02 11:20:11 +0530

    changing json

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 const cors =require('cors')
+const fs = require('fs');
+const path =require('path');
 
 //for CORS either use broser extension or this middleware
 //it will allows from all the clients
@@ -10,12 +12,11 @@ app.use(cors());
 app.get('/api/users',(req,res)=>{
     res.send([
         {
-            name:"Pragati"
-        },{
-            name:"Gaurav"
+            name:"VARUN (pyar se bolun to V.)"
         }
     ]
     )
+   // res.sendFile(path.join(__dirname, '../server/public', 'file.js'));
 })
 app.listen(3000,()=>{
     console.log('server listening on port 3000')
